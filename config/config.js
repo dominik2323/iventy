e40d4e7aade2bf74cc62299d432a73dc1c4f1afd
HAL\Dominik <dominik.tomcik23@gmail.com>
2019-09-13 07:16:38 +0200

    now

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: 'AIzaSyAGgYmng_f35zIiR1Kq5MYFJZ59cQXm5sk',
+  authDomain: 'iventy.firebaseapp.com',
+  databaseURL: 'https://iventy.firebaseio.com',
+  projectId: 'iventy',
+  storageBucket: '',
+  messagingSenderId: '450983222478',
+  appId: '1:450983222478:web:edf47721edfed916'
+};
+
+export default config;
