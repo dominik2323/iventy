399319e5cf82dadf32db4f74e02c81eba14bf7fc
HAL\Dominik <dominik.tomcik23@gmail.com>
2019-09-13 07:51:01 +0200

    env

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ const config = require('./config/config');
 const withSass = require('@zeit/next-sass');
 const withCss = require('@zeit/next-css');
 
-module.exports = withCss(withSass());
+module.exports = withCss(
+  withSass({
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
+  })
+);
